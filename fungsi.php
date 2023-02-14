482e8d3a3907e918f2092dc4dfb3fe0b91ac8c46
saputra <saputraridh@gmail.com>
2023-02-14 15:06:25 +0700

    membuat Shortcode Whatsapp

--- fungsi.php
+++ fungsi.php
@@ -12,4 +12,24 @@ function submenupertama() {
     <p>ini adalah submenu</p>';
 }
 add_action('admin_menu','test_menu_admin');
+
+function fungsi_iklan() {
+    return 'ini adalah iklan';
+}
+
+function whatsapp($atts) {
+    $var = shortcode_atts(
+        array(
+            'nohp' => '085257255213',
+            'pesan' => 'Halo!',
+            'text' => 'Chat Disini'
+        ),$atts
+        );
+        return '<a href="https://wa.me/'.$var['nohp'].'?text='.urlencode($var['pesan']).'" target="_blank">'.$var['text'].'</a>';
+}
+
+add_shortcode('iklan','fungsi_iklan');
+add_shortcode('whatsapp','whatsapp');
+
+
 ?>
